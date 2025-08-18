988f79fab3252f6f83776aed5c876238bea7a6fc
sumayaa7 <sumayaabdurahmonova@gmail.com>
2025-08-18 20:10:17 +0500

    Update hw20.sql

--- lesson-20/homework/hw20.sql
+++ lesson-20/homework/hw20.sql
@@ -55,3 +55,98 @@ FROM #Sales s2
 WHERE s2.CustomerName <> s1.CustomerName
 AND s2.Product = s1.Product
 )
+
+--6.
+SELECT Name,
+ISNULL([Apple], 0)  AS Apple,
+ISNULL([Orange], 0) AS Orange,
+ISNULL([Banana], 0) AS Banana
+FROM 
+(
+SELECT Name, Fruit
+FROM Fruits
+) AS Source
+PIVOT 
+(
+COUNT(Fruit)
+FOR Fruit IN ([Apple], [Orange], [Banana])
+) AS PivotTable
+
+--7.
+create table Family(ParentId int, ChildID int)
+insert into Family values (1, 2), (2, 3), (3, 4)
+select * from Family
+
+-- если перед этим был другой оператор, начни CTE с ;  (важно для T-SQL)
+;WITH FamilyTree AS (
+    -- якорь: прямые связи родитель -> ребёнок
+SELECT ParentId, ChildID
+FROM dbo.Family
+UNION ALL
+
+    -- рекурсия: находим потомков дальше по цепочке
+SELECT f.ParentId, ft.ChildID
+FROM dbo.Family AS f
+JOIN FamilyTree AS ft
+ON f.ChildID = ft.ParentId
+)
+SELECT ParentId AS PID, ChildID AS CHID
+FROM FamilyTree
+GROUP BY ParentId, ChildID          -- на всякий случай убираем дубликаты
+ORDER BY PID, CHID
+OPTION (MAXRECURSION 100);           -- чтоб избежать бесконечных циклов
+
+--8.
+SELECT o.CustomerID, o.OrderID, o.DeliveryState, o.Amount
+FROM #Orders o
+WHERE o.DeliveryState = 'TX'
+AND EXISTS (
+SELECT 1
+FROM #Orders c
+WHERE c.CustomerID = o.CustomerID
+AND c.DeliveryState = 'CA'
+)
+
+--9.
+UPDATE #residents
+SET address = 
+CASE 
+WHEN address NOT LIKE '%name=%' 
+THEN address + ' name=' + fullname
+ELSE address
+END
+select  * from #residents
+
+--10.
+;WITH RoutePaths AS (
+    -- стартуем из Ташкента
+SELECT RouteID, DepartureCity, ArrivalCity,Cost,
+CAST(DepartureCity + ' -> ' + ArrivalCity AS VARCHAR(MAX)) AS Path
+FROM #Routes
+WHERE DepartureCity = 'Tashkent'
+UNION ALL
+    -- продолжаем строить маршрут
+SELECT r.RouteID, rp.DepartureCity, r.ArrivalCity, rp.Cost + r.Cost,
+CAST(rp.Path + ' -> ' + r.ArrivalCity AS VARCHAR(MAX))
+FROM RoutePaths rp
+JOIN #Routes r ON rp.ArrivalCity = r.DepartureCity
+WHERE rp.ArrivalCity <> 'Khorezm'
+),
+Cheapest AS 
+(
+SELECT TOP 1 'Cheapest' AS RouteType, Path, Cost
+FROM RoutePaths
+WHERE ArrivalCity = 'Khorezm'
+ORDER BY Cost ASC
+),
+MostExpensive AS 
+(
+SELECT TOP 1 'Most Expensive' AS RouteType, Path, Cost
+FROM RoutePaths
+WHERE ArrivalCity = 'Khorezm'
+ORDER BY Cost DESC
+)
+SELECT * FROM Cheapest
+UNION ALL
+SELECT * FROM MostExpensive
+
